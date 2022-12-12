0a8bfde5b7b9c8c3746f56157a0f990947f7a095
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-12 15:14:02 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            int flag = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] == a[i - 1])
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 1) textBox2.Text = ("В строке имеются два соседствующих одинаковых символа");
+            else textBox2.Text = ("В строке не имеется два соседствующих одинаковых символа");
+        }
+    }
+}
