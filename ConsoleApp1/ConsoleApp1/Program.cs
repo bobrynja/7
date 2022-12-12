7090be48874c56427f400c5d3665e2f1aa415206
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-12 15:13:29 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string a = Console.ReadLine();
+            
+                int flag = 0;
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if (a[i] == a[i - 1])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 1) Console.WriteLine("В строке имеются два соседствующих одинаковых символа");
+                else Console.WriteLine("В строке не имеется два соседствующих одинаковых символа");
+            
+        }
+    }
+}
