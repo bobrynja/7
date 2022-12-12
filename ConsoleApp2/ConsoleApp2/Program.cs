0836c0a851282913d7037b70a6332621de3b89ca
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-12 15:14:29 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сообщение: ");
+            string b = Console.ReadLine();
+            StringBuilder a = new StringBuilder(b);
+            Console.Write("Введите символ: ");
+            string ch = Console.ReadLine();
+            if (ch == "" || ch == " ") Console.WriteLine("Вы не ввели символ");
+            else
+            {
+
+                string[] c = b.Split(' ', ',', '?', ';', '.', '!');
+
+
+                foreach (string item in c)
+                {
+                    int vv = item.IndexOf(ch.ToUpper());
+                    int xx = item.IndexOf(ch.ToLower());
+                    if (item.IndexOf(ch.ToUpper()) != -1 || item.IndexOf(ch.ToLower()) != -1)
+                    {
+                        int fl = b.IndexOf(item);
+                        a.Remove(fl, item.Length);
+                        b = a.ToString();
+
+                    }
+                }
+                b = b.Trim();
+
+                Console.WriteLine(b);
+            }  
+        }
+    }
+}
